b3f8b638ba9fad2dfab72a1cbc0bf1a81a609f97
Prashanth G <application.stack@gmail.com>
2026-09-12 01:19:56 +0530

    Update rag_chatbot.py

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 EMBEDDING_MODEL = "text-embedding-3-small"
